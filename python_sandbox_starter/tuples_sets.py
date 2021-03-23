14383aa9555110821efbcbf0776c3666cbdd8cf6
Halit Oskan <halit.oskan@and.digital>
2021-03-23 22:56:03 +0000

    tipils

--- python_sandbox_starter/tuples_sets.py
+++ python_sandbox_starter/tuples_sets.py
@@ -1,5 +1,11 @@
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
- 
 
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
+
+new_sets = {"bune", "nope"}
+print("bune" in new_sets)
+
+new_sets.add("iyimis")
+
+print(new_sets)
