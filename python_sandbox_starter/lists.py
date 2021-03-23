1867dfade6026b530fe4ed546f907b46d9eecb8f
Halit Oskan <halit.oskan@and.digital>
2021-03-23 22:18:58 +0000

    pyt trng init

---
+++ python_sandbox_starter/lists.py
@@ -0,0 +1,10 @@
+# A List is a collection which is ordered and changeable. Allows duplicate members.
+
+nums = [1, 3, 5, 7, 9]
+fruits = ["apples", "oranges", "grapes", "pears"]
+
+nums2 = list((2, 4, 6, 8, 10))
+
+print(fruits[1])
+
+print(len(fruits))
---
+++ python_sandbox_starter/variables.py
@@ -0,0 +1,34 @@
+# A variable is a container for a value, which can be of various types
+
+'''
+This is a 
+multiline comment
+or docstring (used to define a functions purpose)
+can be single or double quotes
+'''
+
+"""
+VARIABLE RULES:
+  - Variable names are case sensitive (name and NAME are different variables)
+  - Must start with a letter or an underscore
+  - Can have numbers but can not start with one
+"""
+
+# x = 1
+# y = 2.5
+# name = "John"
+# is_cool = True
+
+# Multiple assignment
+x, y, name, is_cool = (1, 2.5, "john", True)
+
+#print(x, y, name)
+
+# print(type(x))
+
+# Casting
+
+x = str(x)
+y = int(y)
+
+print(type(x))
